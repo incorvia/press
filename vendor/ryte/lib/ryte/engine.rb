b57e53dc356e42eb03e4f77b16a5bdd5f2bfa918
Kevin Incorvia <kevin@incorvia.me>
2012-10-02 10:13:13 -0400

    use rspec and mongoid generators

--- vendor/ryte/lib/ryte/engine.rb
+++ vendor/ryte/lib/ryte/engine.rb
@@ -1,5 +1,10 @@
 module Ryte
   class Engine < ::Rails::Engine
     isolate_namespace Ryte
+
+    config.generators do |g|
+      g.orm             :mongoid
+      g.test_framework  :rspec
+    end
   end
 end
